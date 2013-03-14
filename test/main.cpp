9aed15ce88be4ca0dee2d1b269f9fc4369f28a33
Jacques Rott <jacques.rott@gmail.com>
2013-03-14 21:17:19 +0100

    Test importation "in importation" comment

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,8 @@
 #include <stdlib.h>
-#include <jEngine/plane.h>
 
-
-int main(int argc, char *argv[]) {
-    jE::Plane p(20, 20, 20);
-
-    printf("Tess : %f", p.matrix[0][0]);
+int main() {
+    
+    // see TODO (todo 2)
 
     return 0;
 }
